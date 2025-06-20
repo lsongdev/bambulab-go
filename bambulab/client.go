701b103df4e167e0001073d5a5a02d376d7458b4
Lsong <song940@gmail.com>
2025-06-20 19:13:16 +0800

    update

--- bambulab/client.go
+++ bambulab/client.go
@@ -4,11 +4,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"io"
+	"log"
 	"net/http"
 )
 
+const API = "https://api.bambulab.cn"
+
 type Client struct {
-	client *http.Client
+	client      *http.Client
+	AccessToken string
 }
 
 func NewClient() *Client {
@@ -31,34 +36,105 @@ type LoginResponse struct {
 }
 
 type ErrorResponse struct {
-	Code  int    `json:"code"`
-	Error string `json:"error"`
+	Code    int    `json:"code"`
+	Error   string `json:"error"`
+	Message string `json:"message"`
 }
 
-func (c *Client) Login(credential *Credential) (resp *LoginResponse, err error) {
-	// POST https://api.bambulab.com/v1/user-service/user/login
-	body, err := json.Marshal(credential)
+func (c *Client) request(method, path string, body any) (respBody []byte, err error) {
+	var reqBody io.Reader
+	if body != nil {
+		bodyBytes, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		reqBody = bytes.NewBuffer(bodyBytes)
+	}
+	request, err := http.NewRequest(method, API+path, reqBody)
 	if err != nil {
 		return
 	}
-	req, err := http.NewRequest(http.MethodPost, "https://api.bambulab.com/v1/user-service/user/login", bytes.NewBuffer(body))
+	if c.AccessToken != "" {
+		request.Header.Add("Authorization", "Bearer "+c.AccessToken)
+	}
+	if body != nil {
+		request.Header.Add("content-type", "application/json")
+	}
+	response, err := c.client.Do(request)
 	if err != nil {
 		return
 	}
-	req.Header.Add("content-type", "application/json")
-	res, err := c.client.Do(req)
-	if err != nil {
-		return
-	}
-	if res.StatusCode != 200 {
+	defer response.Body.Close()
+	respBody, err = io.ReadAll(response.Body)
+	// log.Println(response.StatusCode, string(respBody))
+	if response.StatusCode != 200 {
 		errResp := ErrorResponse{}
-		json.NewDecoder(res.Body).Decode(&errResp)
+		json.Unmarshal(respBody, &errResp)
 		err = errors.New(errResp.Error)
 		return
 	}
-	// aaa, _ := io.ReadAll(res.Body)
-	// log.Println(res.StatusCode, string(aaa))
+	return
+}
+
+func (c *Client) Login(credential *Credential) (resp *LoginResponse, err error) {
+	respBody, err := c.request(http.MethodPost, "/v1/user-service/user/login", credential)
+	if err != nil {
+		return
+	}
 	resp = &LoginResponse{}
-	err = json.NewDecoder(res.Body).Decode(&resp)
+	err = json.Unmarshal(respBody, resp)
+	return
+}
+
+type Profile struct {
+	ID              int      `json:"uid"`
+	Account         string   `json:"account"`
+	Name            string   `json:"name"`
+	Avatar          string   `json:"avatar"`
+	FanCount        int      `json:"fanCount"`
+	FollowCount     int      `json:"followCount"`
+	LikeCount       int      `json:"likeCount"`
+	CollectionCount int      `json:"collectionCount"`
+	DownloadCount   int      `json:"downloadCount"`
+	ProductModels   []string `json:"productModels"`
+	Personal        struct {
+		Bio           string   `json:"bio"`
+		Links         []string `json:"links"`
+		BackgroundUrl string   `json:"backgroundUrl"`
+	} `json:"personal"`
+}
+
+func (c *Client) GetProfile() (resp Profile, err error) {
+	body, err := c.request(http.MethodGet, "/v1/user-service/my/profile", nil)
+	if err != nil {
+		return
+	}
+	log.Println(string(body))
+	err = json.Unmarshal(body, &resp)
+	return
+}
+
+type Device struct {
+	ID          string `json:"dev_id"`
+	Name        string `json:"name"`
+	Online      bool   `json:"online"`
+	PrintStatus string `json:"print_status"`
+	Model       string `json:"dev_model_name"`
+	Product     string `json:"dev_product_name"`
+	AccessCode  string `json:"dev_access_code"`
+}
+
+type BindResponse struct {
+	ErrorResponse
+
+	Devices []Device
+}
+
+func (c *Client) ListDevices() (resp BindResponse, err error) {
+	body, err := c.request(http.MethodGet, "/v1/iot-service/api/user/bind", nil)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &resp)
 	return
 }
--- main.go
+++ main.go
@@ -8,13 +8,40 @@ import (
 
 func main() {
 	c := bambulab.NewClient()
-	resp, err := c.Login(&bambulab.Credential{
-		Account:  "song940@gmail.com",
-		Password: "",
-		// Code: "217374",
-	})
+	// resp, err := c.Login(&bambulab.Credential{
+	// 	Account: "song940@gmail.com",
+	// 	// Password: "lsong940",
+	// 	Code: "712362",
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// 2025/06/20 14:42:04 &{AAC23s0DhD2q8PB5ndMV9--VcY6TvJlcSQVOmg6pjnGCu3dZNbQUQ7rIp43S5yTPwJbWIXSQN5IgpeBnt3cy7rRdIqmbdM5GRLaW8i62HEQoom8HzoW5H6Ymk9PVT1l_upxMpTpKM1W-3HLU AAC23s0DhD2q8PB5ndMV9--VcY6TvJlcSQVOmg6pjnGCu3dZNbQUQ7rIp43S5yTPwJbWIXSQN5IgpeBnt3cy7rRdIqmbdM5GRLaW8i62HEQoom8HzoW5H6Ymk9PVT1l_upxMpTpKM1W-3HLU  7776000}
+	// c.AccessToken = "AAC23s0DhD2q8PB5ndMV9--VcY6TvJlcSQVOmg6pjnGCu3dZNbQUQ7rIp43S5yTPwJbWIXSQN5IgpeBnt3cy7rRdIqmbdM5GRLaW8i62HEQoom8HzoW5H6Ymk9PVT1l_upxMpTpKM1W-3HLU"
+
+	// resp1, err := c.Login(&bambulab.Credential{
+	// 	Account: "18510100102",
+	// 	// Password: "lsong940!",
+	// 	Code: "808975",
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// 2025/06/20 17:18:47 &{AACsYzLDNxyC1u1HbeiegeMaONXU4UVkKlLcrBkJOitmmBMMAGnIZbDp9T5CV_ZsyxawsbMfyjYRBKBWPRGvqjBoSiLBJVszMbYKIVvsS_MlSEkhvxZ3TsfSGbePGgv4OdqWFEWlE_VhnoUP AACsYzLDNxyC1u1HbeiegeMaONXU4UVkKlLcrBkJOitmmBMMAGnIZbDp9T5CV_ZsyxawsbMfyjYRBKBWPRGvqjBoSiLBJVszMbYKIVvsS_MlSEkhvxZ3TsfSGbePGgv4OdqWFEWlE_VhnoUP  7776000}
+	// log.Println(resp1)
+	c.AccessToken = "AACsYzLDNxyC1u1HbeiegeMaONXU4UVkKlLcrBkJOitmmBMMAGnIZbDp9T5CV_ZsyxawsbMfyjYRBKBWPRGvqjBoSiLBJVszMbYKIVvsS_MlSEkhvxZ3TsfSGbePGgv4OdqWFEWlE_VhnoUP"
+
+	profile, err := c.GetProfile()
 	if err != nil {
 		panic(err)
 	}
-	log.Println(resp)
+	log.Println(profile)
+
+	resp2, err := c.ListDevices()
+	if err != nil {
+		panic(err)
+	}
+	for _, device := range resp2.Devices {
+		log.Println(device.ID, device.Name, device.Online, device.Model, device.Product, device.AccessCode)
+	}
 }
